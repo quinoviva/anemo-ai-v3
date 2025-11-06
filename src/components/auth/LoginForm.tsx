eee5c1378ac830f34e9b60cce2a3390a538e6cc1
Cyril Quinoviva <cyquinoviva@gmail.com>
2025-11-06 16:22:09 +0000

    conitnue with google or contonue as guest

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -170,7 +170,7 @@ export function LoginForm() {
           </div>
           <div className="relative flex justify-center text-xs uppercase">
             <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
+              Or
             </span>
           </div>
         </div>
