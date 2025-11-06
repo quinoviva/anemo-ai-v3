dba6f22a5e42d85373188c99c494c9d6e424faaf
Cyril Quinoviva <cyquinoviva@gmail.com>
2025-11-06 15:52:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5574929814-ea244",
+  "appId": "1:33629963873:web:855a8de70da9082036cc20",
+  "apiKey": "AIzaSyDRt82D8vWouBq1TVmAn9xZDPyVAo-N0TY",
+  "authDomain": "studio-5574929814-ea244.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "33629963873"
+};
