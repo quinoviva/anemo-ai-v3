26240a593c55ea19d9969b88af9293a498fcf20f
Cyril Quinoviva <cyquinoviva@gmail.com>
2025-11-07 04:54:45 +0000

    Terms of Services and Privacy Policy. put this below new line (_for elem

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -318,7 +318,14 @@ export function SignUpForm() {
         </CardContent>
       </Card>
       <p className="mt-4 px-8 text-center text-xs text-muted-foreground">
-        By continuing, you agree to our Terms of Service and Privacy Policy
+        By continuing, you agree to our <br />
+        <Link href="#" className="underline underline-offset-4 hover:text-primary">
+            Terms of Service
+        </Link>
+        {' '}and{' '}
+        <Link href="#" className="underline underline-offset-4 hover:text-primary">
+            Privacy Policy
+        </Link>
       </p>
     </div>
   );
